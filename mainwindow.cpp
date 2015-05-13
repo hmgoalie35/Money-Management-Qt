e13ce61dd3d35ded781d6c3501d467baad934c95
Harris Pittinsky <hpittin1@binghamton.edu>
2015-05-13 12:27:34 -0400

    create the layout. Functionality WIP.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QIcon>
+#include <iostream>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -8,9 +9,27 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     setWindowIcon(QIcon(":/imgs/money_management.gif"));
     setWindowTitle("Money Management");
+    setFixedSize(width(), height());
+
+    status_bar_label = new QLabel();
+    ui->statusBar->addPermanentWidget(status_bar_label, width());
+    status_bar_label->setText("[+] Status");
+
+    ui->dateEdit->setDate(QDate::currentDate());
+
+    QList<QString> options;
+    options.append("Deposit");
+    options.append("Withdraw");
+    ui->comboBoxMode->addItems(options);
+    ui->comboBoxMode->setCurrentIndex(-1);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionAbout_triggered()
+{
+  std::cout << "About action" << std::endl;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QLabel>
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +15,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+  void on_actionAbout_triggered();
+
 private:
     Ui::MainWindow *ui;
+    QLabel* status_bar_label;
 };
 
 #endif // MAINWINDOW_H
